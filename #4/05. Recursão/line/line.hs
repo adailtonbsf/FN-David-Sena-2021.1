2b8a9be67fb5aa89254088a5c6d3a0a53b4b1004
Adailton <adailtonbsf@gmail.com>
2021-06-15 08:14:12 -0300

    ⠀

--- #4/05. Recursão/line/line.hs
+++ #4/05. Recursão/line/line.hs
@@ -1,5 +1,11 @@
-line a = [soma (a - 1) + 1.. soma a]
-    where soma b = sum [1..b]
+line' a b
+    | a > 0 = (b+1):line' (a-1) (b+1)
+    | otherwise = []
+
+line a = soma:line' (a-1) soma
+    where
+        b = a - 1
+        soma = sum [1..b] + 1
 
 main = do
     a <- readLn :: IO Int
--- #4/05. Recursão/triangle/triangle.hs
+++ #4/05. Recursão/triangle/triangle.hs
@@ -1,6 +1,17 @@
-line a = [soma (a - 1) + 1.. soma a]
-    where soma b = sum [1..b]
-triangle a = [line b | b <- [1..a]]
+line' a b
+    | a > 0 = (b+1):line' (a-1) (b+1)
+    | otherwise = []
+
+line a = soma:line' (a-1) soma
+    where
+        b = a - 1
+        soma = sum [1..b] + 1
+
+triangle' a b
+    | b <= a = line b:triangle' a (b+1)
+    | otherwise = []
+
+triangle a = triangle' a 1
 
 main = do
     a <- readLn :: IO Int
